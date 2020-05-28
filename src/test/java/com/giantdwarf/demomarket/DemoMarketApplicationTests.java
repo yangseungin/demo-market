f68ad0c1801f40a60c1d91f2ae5e5ffee5dc6b72
seungin yang <rhfpdk92@naver.com>
2020-05-28 12:03:28 +0900

    Initial commit

---
+++ src/test/java/com/giantdwarf/demomarket/DemoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giantdwarf.demomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
