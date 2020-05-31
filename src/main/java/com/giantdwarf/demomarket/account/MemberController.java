fba5960b54640a8b84a2680254173777da78f43a
seungin yang <rhfpdk92@naver.com>
2020-06-01 01:31:58 +0900

    password 암호화 회원가입 제약조건추가

--- src/main/java/com/giantdwarf/demomarket/account/MemberController.java
+++ src/main/java/com/giantdwarf/demomarket/account/MemberController.java
@@ -3,13 +3,17 @@ package com.giantdwarf.demomarket.account;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.BindResult;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,19 +21,25 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @Autowired
-    MemberRepository memberRepository;
-
     @GetMapping("/member/signup")
-    public String signupForm(Model model){
+    public String signupForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "member/signup";
     }
 
     @PostMapping("/member/signup")
-    public String creatae(@Valid MemberForm form){
+    public String creatae(@Valid MemberForm form, BindingResult result) {
+
+        if(result.hasErrors()){
+            return "member/signup";
+        }
+
+        //패스워드 암호화
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+
         Member member = Member.builder().memberId(form.getMemberId())
-                .password(form.getPassword())
+                .password(passwordEncoder.encode(form.getPassword()))
                 .email(form.getEmail())
                 .name(form.getName())
                 .updateDate(LocalDateTime.now())
@@ -39,6 +49,12 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping("member/list")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "member/memberList";
+    }
 
 
 }
--- src/main/java/com/giantdwarf/demomarket/account/MemberForm.java
+++ src/main/java/com/giantdwarf/demomarket/account/MemberForm.java
@@ -1,18 +1,25 @@
 package com.giantdwarf.demomarket.account;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
 @Data
 public class MemberForm {
-//    @NotEmpty(message = "회원 id는 필수")
+//    @NotBlank
     private String memberId;
 
+//    @NotBlank
     private String password;
 
+//    @NotBlank
     private String name;
 
+//    @Email
+//    @NotBlank
     private String email;
 }
